0da55a0eec0b83c7626ad4b6d0f1cfc7de0f6b7e
Carolina <csolorzanoomar1419@gmail.com>
2026-01-22 12:26:46 +0100

    creacion de product

---
+++ src/model/Product.java
@@ -0,0 +1,94 @@
+package model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double publicPrice;
+    private double wholesalerPrice;
+    private boolean available;
+    private int stock;
+    private static int totalProducts;
+    
+    public static double EXPIRATION_RATE = 0.60;
+
+    public Product(String name, double wholesalerPrice, boolean available, int stock) {
+        super();
+        this.id = totalProducts + 1;
+        this.name = name;
+        this.wholesalerPrice = wholesalerPrice;
+        this.publicPrice = wholesalerPrice *2;
+        this.available = available;
+        this.stock = stock;
+        totalProducts++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPublicPrice() {
+        return publicPrice;
+    }
+
+    public void setPublicPrice(double publicPrice) {
+        this.publicPrice = publicPrice;
+    }
+
+    public double getWholesalerPrice() {
+        return wholesalerPrice;
+    }
+
+    public void setWholesalerPrice(double wholesalerPrice) {
+        this.wholesalerPrice = wholesalerPrice;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public static int getTotalProducts() {
+        return totalProducts;
+    }
+
+    public static void setTotalProducts(int totalProducts) {
+        Product.totalProducts = totalProducts;
+    }
+
+    public void expire() {
+        this.publicPrice = this.getPublicPrice() * EXPIRATION_RATE;
+    }
+
+    public void setAvailabe(boolean b) {
+        // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + "  | Product: " + name + " | Price: " + publicPrice + " | Stock: " + stock + " |  Disponibilidad: " + available + " |   Precio Mayorista: " + wholesalerPrice + ".";
+    }
+   
+}
